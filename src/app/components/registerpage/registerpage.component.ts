ce3dd76d35f9c76cfee4bec7654b6d13ee517481
Alison Rodrigues <alison.si1302@gmail.com>
2023-06-21 23:37:17 -0300

    Register Page

--- src/app/components/registerpage/registerpage.component.ts
+++ src/app/components/registerpage/registerpage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registerpage',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./registerpage.component.css']
 })
 export class RegisterpageComponent {
+
+
+  nameFormControl = new FormControl('', [Validators.required, Validators.maxLength(150)]);
+  lastNameFormControl = new FormControl('', [Validators.required, Validators.maxLength(150)]);
+  emailFormControl = new FormControl('', [Validators.required, Validators.email, Validators.maxLength(30)]);
+  cpfFormControl = new FormControl('', [Validators.required, Validators.maxLength(11)]);
+  cepFormControl = new FormControl('', [Validators.required, Validators.maxLength(8)]);
+  streetFormControl = new FormControl('', [Validators.required, Validators.maxLength(150)]);
+  neighborhoodFormControl = new FormControl('', [Validators.required, Validators.maxLength(150)]);
+
 
 }
